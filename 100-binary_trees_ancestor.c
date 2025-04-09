d5ccf73a5ae2c6194c8069e6e5d4568d2097c75c
federico-paganini <perfedefer@gmail.com>
2025-04-09 23:10:46 +0000

    [FEAT] depth function add

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -43,3 +43,27 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	return (NULL);
 }
+
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree.
+ *
+ * @tree: Pointer to the node.
+ *
+ * Return: Deep of the node.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
