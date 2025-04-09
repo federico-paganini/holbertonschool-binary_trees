b462f58ebd0b32f355a667ad74780178b818c8c4
federico-paganini <perfedefer@gmail.com>
2025-04-09 21:29:56 +0000

    [FIX] Calculate Heigth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,13 +11,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t height1 = 0, heigth2 = 0;
 
 	if (!tree)
 		return (0);
 
-	height += tree->right ? 1 : 0;
-	height += tree->left ? 1 : 0;
+	if (!tree->left && !tree->rigth)
+		return (0);
 
-	return (height);
+	height1 = binary_tree_height(tree->left);
+	height2 = binary_tree_heigth(tree->left);
+
+	return (1 + (height1 > heigth2 ? heigth1 : heigth2));
 }
