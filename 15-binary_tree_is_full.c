3a4868a31c6c6c556e399728351ec0a144c1dc5d
federico-paganini <perfedefer@gmail.com>
2025-04-09 22:19:59 +0000

    [FIX] type

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,7 +18,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 
-	if (tree->rigth && tree->left)
+	if (tree->right && tree->left)
 		return (binary_tree_is_full(tree->left) &&
 				binary_tree_is_full(tree->right));
 
